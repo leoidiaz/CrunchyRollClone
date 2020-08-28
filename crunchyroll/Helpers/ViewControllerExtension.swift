ecdc18459cfa83cbd9db19219b78b9b690cca6a9
leoidiaz <zaid.leonardo@gmail.com>
2020-08-28 15:10:45 -0700

    add app wide error alert controller

--- crunchyroll/Helpers/ViewControllerExtension.swift
+++ crunchyroll/Helpers/ViewControllerExtension.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2020 Leonardo Diaz. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func presentErrorToUser(title: String, localizedError: CRError) {
+        let alertController = UIAlertController(title: title, message: localizedError.errorDescription, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Ok", style: .cancel)
+        alertController.addAction(dismissAction)
+        present(alertController, animated: true)
+    }
+}
