79f0178c2bc574c624d0da03e2e35c32766faf06
leoidiaz <zaid.leonardo@gmail.com>
2020-08-28 15:10:08 -0700

    update model

--- crunchyroll/Model/Anime.swift
+++ crunchyroll/Model/Anime.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class TopLevelObject: Decodable {
+    let data: [Anime]
+}
+
+class Anime: Decodable {
+    let id: String
+    let attributes: Attributes
+}
+
+class Attributes: Decodable {
+    let canonicalTitle: String
+    let subtype: String
+    let description: String
+    let posterImage: PosterImage
+}
+
+class PosterImage: Decodable {
+    let medium: String
+}
