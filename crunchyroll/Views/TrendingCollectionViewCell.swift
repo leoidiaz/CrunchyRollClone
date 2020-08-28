2cf3c7743055ed9f8d77a96892e2e4300d67711b
leoidiaz <zaid.leonardo@gmail.com>
2020-08-28 15:12:26 -0700

    trending collection view setup

--- crunchyroll/Views/TrendingCollectionViewCell.swift
+++ crunchyroll/Views/TrendingCollectionViewCell.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class TrendingCollectionViewCell: UICollectionViewCell {
+    //MARK: - Outlets
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var moreInfoButton: UIButton!
     
+    //MARK: - Properties
+    var anime: Anime? {
+        didSet{
+            setupCell()
+        }
+    }
+    //MARK: - Helper Methods
+    private func setupCell(){
+        guard let anime = anime else { return }
+        titleLabel.text = anime.attributes.canonicalTitle
+        typeLabel.text = anime.attributes.subtype
+        posterImageView.image = nil
+        AnimeController.fetchPoster(posterPath: anime.attributes.posterImage.medium) { [weak self] (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let poster):
+                    self?.posterImageView.image = poster
+                case .failure(let error):
+                    return print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                }
+            }
+        }
+    }
 }
