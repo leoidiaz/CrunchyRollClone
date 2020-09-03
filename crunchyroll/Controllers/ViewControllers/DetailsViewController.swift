e0d99d17eb1e97e42bd64d324339a8f693917ba4
leoidiaz <zaid.leonardo@gmail.com>
2020-09-03 15:24:03 -0700

    Create DetailsViewController

---
+++ crunchyroll/Controllers/ViewControllers/DetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsViewController.swift
+//  crunchyroll
+//
+//  Created by Leonardo Diaz on 9/3/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
