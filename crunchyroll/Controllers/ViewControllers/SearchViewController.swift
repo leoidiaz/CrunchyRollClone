a268354fed1084fde7925936fd1666c6104aa960
leoidiaz <zaid.leonardo@gmail.com>
2020-09-18 15:32:44 -0700

    Create SearchViewController

---
+++ crunchyroll/Controllers/ViewControllers/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  crunchyroll
+//
+//  Created by Leonardo Diaz on 9/16/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
