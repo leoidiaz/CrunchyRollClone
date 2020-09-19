879747d1cba7a964a7bbb5a715dba15d02c01a4c
leoidiaz <zaid.leonardo@gmail.com>
2020-09-19 16:39:23 -0700

    Create SearchTableViewCell

---
+++ crunchyroll/Views/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  crunchyroll
+//
+//  Created by Leonardo Diaz on 9/18/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
