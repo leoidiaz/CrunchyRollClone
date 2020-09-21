4ad225f312a3b44b173c40990b14b4f8bd000163
leoidiaz <zaid.leonardo@gmail.com>
2020-09-21 15:31:13 -0700

    SearchViewController setup

--- crunchyroll/Controllers/ViewControllers/SearchViewController.swift
+++ crunchyroll/Controllers/ViewControllers/SearchViewController.swift
@@ -9,22 +9,87 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    //MARK: - Outlets
+    @IBOutlet weak var animeQueryBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    var animes = [Anime]()
+    private let reuseID = "searchedAnime"
+    private let segueIdentifier = "detailsFromSearchVC"
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
+    }
+    //MARK: - Actions
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    //MARK: - Helper Methods
+    private func fetchAnime(anime: String){
+        AnimeController.fetchAnimes(searchType: .query, query: anime, idURL: nil) { [weak self] (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let animes):
+                    self?.animes = animes
+                    self?.tableView.reloadData()
+                case .failure(let error):
+                    self?.presentErrorToUser(title: "Search Error", localizedError: .thrownError(error))
+                }
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupView(){
+        animeQueryBar.becomeFirstResponder()
+        tableView.delegate = self
+        tableView.dataSource = self
+        animeQueryBar.delegate = self
     }
-    */
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueIdentifier {
+            guard let indexPath = tableView.indexPathForSelectedRow, let destinationVC = segue.destination as? DetailsViewController else { presentErrorToUser(title: "Unable to Segue", localizedError: .noNetwork) ; return }
+            let anime = animes[indexPath.row]
+            destinationVC.anime = anime
+            tableView.deselectRow(at: indexPath, animated: false)
+        }
+    }
 
+}
+
+    //MARK: - Tableview Delegates
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath) as? SearchTableViewCell else { return UITableViewCell()}
+        let anime = animes[indexPath.row]
+        cell.anime = anime
+        return cell
+    }
+}
+
+    //MARK: - SearchBar Delegate
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text, !searchTerm.isEmpty else { return }
+        fetchAnime(anime: searchTerm)
+        resultsLabel.isHidden = false
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text else { return}
+        if searchText.isEmpty{
+            resultsLabel.isHidden = true
+            animes.removeAll()
+            tableView.reloadData()
+        }
+    }
 }
