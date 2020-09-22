9e93da117c5ec4388fdacdf9c69df489dd5e5a1f
leoidiaz <zaid.leonardo@gmail.com>
2020-09-22 14:41:21 -0700

    crunchyroll/Model/User.swift

--- crunchyroll/Model/User.swift
+++ crunchyroll/Model/User.swift
@@ -10,36 +10,27 @@ import Foundation
 
 struct UserKeys {
     static let documentKey = "users"
-    static let emailKey = "email"
     static let myLists = "myList"
 }
 
 class User: Codable {
-    var email: String
-    var myLists: [String] = []
+    var myList: [String]
     
-    init(email: String, password: String) {
-        self.email = email
+    init(myList: [String] = []) {
+        self.myList = myList
     }
     
     init?(userDictionary: [String: Any]) {
-        guard let email = userDictionary[UserKeys.emailKey] as? String else { fatalError("Can not find email address.") }
         guard let myLists = userDictionary[UserKeys.myLists] as? [String] else { fatalError("Can not find documents")}
-        self.email = email
-        self.myLists = myLists
+        self.myList = myLists
     }
-}
-
-extension User: Equatable {
-    static func ==(lhs: User, rhs: User) -> Bool {
-        return lhs.email == rhs.email
-    }
+    
 }
 
 extension User {
     var documentDictionary: [String:Any] {
         let dictionary: [String:Any] = [
-            UserKeys.myLists : myLists
+            UserKeys.myLists : myList
         ]
         return dictionary
     }
