e31b57490c7f15500556e5fd7cea3df261d27e68
leoidiaz <zaid.leonardo@gmail.com>
2020-09-25 13:00:41 -0700

    functional mylist

--- crunchyroll/Controllers/ViewControllers/MyListsTableViewController.swift
+++ crunchyroll/Controllers/ViewControllers/MyListsTableViewController.swift
@@ -9,82 +9,66 @@
 import UIKit
 
 class MyListsTableViewController: UITableViewController {
-
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        fetchMyList()
+    }
+    
+    //MARK: - Properties
+    private let reuseIdentifier = "myListCells"
+    private let segueIdentifier = "detailsFromMyListVC"
+    
+    var animes = [Anime]() {
+        didSet{
+            if animes.count == UserController.shared.mylist.count {
+                tableView.reloadData()
+            }
+        }
+    }
+    
+    //MARK: - Helper Methods
+    
+    @objc private func fetchMyList(){
+        animes.removeAll()
+        guard !UserController.shared.mylist.isEmpty else { return }
+        for animeURL in UserController.shared.mylist {
+            AnimeController.fetchMyListAnime(idURL: animeURL) { [weak self] (result) in
+                DispatchQueue.main.sync {
+                    switch result {
+                    case .success(let anime):
+                        self?.animes.append(anime)
+                    case .failure(let error):
+                        self?.presentErrorToUser(title: "Unable to retrieve my list", localizedError: .thrownError(error))
+                    }
+                }
+            }
+        }
+    }
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return animes.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? SearchTableViewCell else { return UITableViewCell()}
+        let anime = animes[indexPath.row]
+        cell.anime = anime
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == segueIdentifier {
+            guard let indexPath = tableView.indexPathForSelectedRow, let destinationVC = segue.destination as? DetailsViewController else { presentErrorToUser(title: "Unable to Segue", localizedError: .noNetwork) ; return }
+            let anime = animes[indexPath.row]
+            destinationVC.myList = UserController.shared.mylist
+            destinationVC.anime = anime
+            tableView.deselectRow(at: indexPath, animated: false)
+        }
     }
-    */
-
 }
