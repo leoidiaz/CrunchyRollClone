5ab2d9374dcb89cf58c1c2774e9347def204e323
leoidiaz <zaid.leonardo@gmail.com>
2020-09-27 11:06:12 -0700

    setup coverImageCollectionCell

--- crunchyroll/Views/CoverCollectionViewCell.swift
+++ crunchyroll/Views/CoverCollectionViewCell.swift
@@ -9,5 +9,30 @@
 import UIKit
 
 class CoverCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+    var anime: Anime? {
+        didSet{
+            setupCell()
+        }
+    }
+    
+    //MARK: - Helper Methods
+    private func setupCell(){
+        coverImageView.image = nil
+        coverImageView.layer.cornerRadius = 5
+        guard let anime = anime else { return }
+        guard let animeCover = anime.attributes.coverImage, let tinyCover = animeCover.tiny else { return }
+        AnimeController.fetchPoster(posterPath: tinyCover) { [weak self] (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let poster):
+                    self?.coverImageView.image = poster
+                case .failure(let error):
+                    return print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                }
+            }
+        }
+    }
     
 }
