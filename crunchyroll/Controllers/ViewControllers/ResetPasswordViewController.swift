c81ea6cb8ed8400475bcbb5b0148df5fccaaa582
leoidiaz <zaid.leonardo@gmail.com>
2020-10-07 16:54:39 -0700

    Create ResetPasswordViewController

---
+++ crunchyroll/Controllers/ViewControllers/ResetPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResetPasswordViewController.swift
+//  crunchyroll
+//
+//  Created by Leonardo Diaz on 10/7/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
