d9bdebe60efb7eb363598a792eeb27695566edd8
leoidiaz <zaid.leonardo@gmail.com>
2020-10-07 14:49:33 -0700

    added animeController tests

--- crunchyrollTests/AnimeControllerTests.swift
+++ crunchyrollTests/AnimeControllerTests.swift
@@ -5,29 +5,69 @@
 //  Created by Leonardo Diaz on 10/7/20.
 //  Copyright © 2020 Leonardo Diaz. All rights reserved.
 //
-
+@testable import crunchyroll
 import XCTest
 
 class AnimeControllerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var animeController: AnimeController!
+    let posterPath = "https://media.kitsu.io/anime/poster_images/1/medium.jpg?1597604210"
+    override func setUp() {
+        super.setUp()
+        animeController = AnimeController()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
+        animeController = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    
+    func testGet_Poster(){
+        let expectation = XCTestExpectation(description: "Wait for network")
+        animeController.getPoster(posterPath: posterPath) { (result) in
+            switch result {
+            case .success(let poster):
+                XCTAssertNotNil(poster)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Unable to fetch anime from mylist url, \(error.localizedDescription)")
+                expectation.fulfill()
+            }
         }
+        wait(for: [expectation], timeout: 30.0)
     }
-
+    
+    func testGet_Animes(){
+        let expectation = XCTestExpectation(description: "Wait for network")
+        animeController.getAnimes(searchType: .trending, query: nil) { (result) in
+            switch result {
+            case .success(let animes):
+                XCTAssertNotNil(animes)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Unable to fetch anime from mylist url, \(error.localizedDescription)")
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation], timeout: 30.0)
+    }
+    
+    func testGet_Anime(){
+        let expectation = XCTestExpectation(description: "Wait for network")
+        animeController.getMyListAnime(idURL: "1") { (result) in
+            switch result {
+            case .success(let anime):
+                XCTAssertNotEqual(anime.id, "")
+                XCTAssertEqual(anime.attributes.canonicalTitle, "Cowboy Bebop")
+                XCTAssertEqual(anime.attributes.subtype, "TV")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Unable to fetch anime from mylist url, \(error.localizedDescription)")
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation], timeout: 30.0)
+    }
+    
 }
