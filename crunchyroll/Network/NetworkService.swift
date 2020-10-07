c6417d0a32051a6c4434fa884c2f0960b2348788
leoidiaz <zaid.leonardo@gmail.com>
2020-10-07 14:45:47 -0700

    refactor method type in network service

--- crunchyroll/Network/NetworkService.swift
+++ crunchyroll/Network/NetworkService.swift
@@ -9,14 +9,14 @@
 import Foundation
 
 class NetworkService {
-     static private let baseURL = URL(string: "https://kitsu.io/api/edge")
-     static private let trendingKey = "trending"
-     static private let subKey = "anime"
+      private let baseURL = URL(string: "https://kitsu.io/api/edge")
+      private let trendingKey = "trending"
+      private let subKey = "anime"
     //https://kitsu.io/api/edge/anime?filter[text]=demon%20slayer
-     static private let queryKey = "filter[text]"
+      private let queryKey = "filter[text]"
     //https://kitsu.io/api/edge/anime?page[limit]=5&page[offset]=12062
 
-    static func fetchAnimes(searchType: AnimeFetchType, query: String?, completion: @escaping(Result<Data,CRError>) -> Void) {
+     func fetchAnimes(searchType: AnimeFetchType, query: String?, completion: @escaping(Result<Data,CRError>) -> Void) {
         guard let baseURL = baseURL else { return completion(.failure(.invalidURL)) }
         var request: URLRequest
         switch searchType {
@@ -35,7 +35,7 @@ class NetworkService {
         resumeDataTask(withRequest: request, completion: completion)
     }
     
-     static func fetchMyListAnime(idURL: String?, completion: @escaping(Result<Data, CRError>) -> Void) {
+      func fetchMyListAnime(idURL: String?, completion: @escaping(Result<Data, CRError>) -> Void) {
         guard let baseURL = baseURL else { return completion(.failure(.invalidURL)) }
         guard let idKey = idURL else { return completion(.failure(.noData))}
         let subtype = baseURL.appendingPathComponent(subKey)
@@ -44,14 +44,13 @@ class NetworkService {
         resumeDataTask(withRequest: request, completion: completion)
     }
     
-    
-     static func fetchPoster(posterPath: String, completion: @escaping(Result<Data, CRError>) -> Void){
+      func fetchPoster(posterPath: String, completion: @escaping(Result<Data, CRError>) -> Void){
         guard let posterURL = URL(string: posterPath) else { return completion(.failure(.invalidURL))}
         let request = URLRequest(url: posterURL)
         resumeDataTask(withRequest: request, completion: completion)
     }
     
-     static func resumeDataTask(withRequest request: URLRequest, completion: @escaping(Result<Data, CRError>) -> Void){
+      func resumeDataTask(withRequest request: URLRequest, completion: @escaping(Result<Data, CRError>) -> Void){
         let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
