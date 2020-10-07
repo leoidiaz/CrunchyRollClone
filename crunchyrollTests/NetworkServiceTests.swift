90569bea2fa02bdc0fd592580f6f968193eb0a4d
leoidiaz <zaid.leonardo@gmail.com>
2020-10-07 14:45:13 -0700

    refactor method type in network service

--- crunchyrollTests/NetworkServiceTests.swift
+++ crunchyrollTests/NetworkServiceTests.swift
@@ -6,28 +6,95 @@
 //  Copyright © 2020 Leonardo Diaz. All rights reserved.
 //
 
+@testable import crunchyroll
 import XCTest
 
 class NetworkServiceTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let baseURL = URL(string: "https://kitsu.io/api/edge")
+    let posterPath = "https://media.kitsu.io/anime/poster_images/1/medium.jpg?1597604210"
+    let trendingKey = "trending"
+    let subKey = "anime"
+    var network: NetworkService!
+    
+    override func setUp() {
+        super.setUp()
+        network = NetworkService()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
+        network = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testData_Task() {
+        let expectation = XCTestExpectation(description: "Wait for network")
+        guard let baseURL = baseURL else { XCTFail("Unable to turn baseURL string to URL") ; return }
+        let trendingType = baseURL.appendingPathComponent(trendingKey)
+        let finalURL = trendingType.appendingPathComponent(subKey)
+        let urlRequest = URLRequest(url: finalURL)
+        network.resumeDataTask(withRequest: urlRequest) { (result) in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Unable to fetch animes from url, \(error.localizedDescription)")
+                expectation.fulfill()
+            }
         }
+        wait(for: [expectation], timeout: 10.0)
     }
-
+    
+    func testCheck_URL() {
+        guard let baseURL = baseURL else { XCTFail("Unable to turn baseURL string to URL") ; return }
+        let trendingType = baseURL.appendingPathComponent(trendingKey)
+        let finalURL = trendingType.appendingPathComponent(subKey)
+        XCTAssertEqual(finalURL, URL(string: "https://kitsu.io/api/edge/trending/anime"))
+    }
+    
+    
+    func testFetch_trending() {
+        let expectation = XCTestExpectation(description: "Wait for network")
+        network.fetchAnimes(searchType: .trending, query: nil) { (result) in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Unable to fetch animes from url, \(error.localizedDescription)")
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation], timeout: 30.0)
+    }
+    
+    func testFetchMyListAnime() {
+        let expectation = XCTestExpectation(description: "Wait for network")
+        network.fetchMyListAnime(idURL: "1") { (result) in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Unable to fetch anime from mylist url, \(error.localizedDescription)")
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation], timeout: 30.0)
+    }
+    
+    func testFetchPoster() {
+        let expectation = XCTestExpectation(description: "Wait for network")
+        network.fetchPoster(posterPath: posterPath) { (result) in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Unable to fetch anime from mylist url, \(error.localizedDescription)")
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation], timeout: 30.0)
+    }
 }
